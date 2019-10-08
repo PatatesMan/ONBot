01b35cde88cd658ec8024beba0bdfe186ad149ff
PatatesMan <54624713+PatatesMan@users.noreply.github.com>
2019-10-08 15:45:49 +0200

    Create davet.js

---
+++ komutlar/davet.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+exports.run = (client, message) => {
+  if (message.channel.type !== 'dm') {
+    const ozelmesajkontrol = new Discord.RichEmbed()
+    .setColor("RANDOM")
+    .setTimestamp()
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .setDescription('**__Özel__ __mesaj__larını kontrol et.** :postbox:');
+    message.channel.sendEmbed(ozelmesajkontrol) }
+	const pingozel = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTimestamp()
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .setDescription(`\n**Davet __Link__'im İçin Tıkla** [Davet Link](botun davet linki)`);
+    return message.author.sendEmbed(pingozel)
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['botu ekle', 'botu davet et', 'botuekle', 'invite'],
+  permLevel: 0,
+  kategori: "bot"
+};
+
+exports.help = {
+  name: 'davet',
+  description: 'Botun davet linkini gönderir.',
+  usage: 'davet'
+};
